8d1f5607b05c8af9795e2f71b74e53bb55b0a7e3
IcWDQ <142859645+IcWDQ@users.noreply.github.com>
2026-03-26 12:40:49 +0800

    Add PostgreSQL Sequelize configuration

---
+++ server/src/config/database.ts
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize');
+
+// Sequelize configuration for PostgreSQL
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'postgres',
+});
+
+// Test the connection
+sequelize.authenticate().then(() => {
+  console.log('Connection has been established successfully.');
+}).catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
+
+module.exports = sequelize;
